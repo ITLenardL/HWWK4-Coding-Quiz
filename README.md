e09464e9e883ab7d1c0eb5a2b732b74ef92ea5d9
lenny <llaska@live.com>
2020-12-07 23:40:14 +1100

    updated all files to complete project, readme pictures

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# HWWK4
+# HWWK4 - Code Quiz
+Tasked with creating a coding quiz - asks a bunch of questions relating to code, runs down a timer that will be deducted for each incorrect answer. The game will end if timer hits 0.
+At the end the score will be saved to local memory to be brought up when viewing high score.
+
+# Example
+The quiz should work as below
+(Example/image.png), (Example/image (1).png), (Example/image (2).png), (Example/image (3).png)
+<br>
+
+# Problems
+Sometimes the score doesn't align with the timer's remaining time, I suspect that the final question executes the end game before the score can be determined, or something to that extent. Have only coded 1 stored score, should have created a list element to stack previous scores so as to compare to everyone who's played.
--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-var questions = [{
-    title: "Which of the following function of an array object adds one or more elements to the front of an array and returns the new length of the array?",
-    choices: ["unshift( )", "sort( )", "splice( )", "toString( )"],
-    answer: "unshift( )"
-},
-{
-    title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
-    choices: ["last( )", "put( )", "push( )", "pop( )"],
-    answer: "push( )"
-},
-{
-    title: " Which built-in method returns the characters in a string beginning at the specified location?",
-    choices: ["substr( )", "getSubstring( )", "slice( )", "None of the above."],
-    answer: "substr( )"
-},
-{
-    title: "Which of the following function of an array object adds and/or removes elements from an array?",
-    choices: ["toSource( )", "sort( )", "unshift( )", "splice( )"],
-    answer: "splice( )"
-},
-{
-    title: "Which of the following function of String object combines the text of two strings and returns a new string?",
-    choices: ["add( )", "concat( )", " merge( )", "append( )"],
-    answer: "concat( )"
-}
-]
+
+//question variable arrays for quiz
+var questions = [
+    {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+    },
+    {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+    },
+    {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings","other arrays","booleans","all of the above",],
+    answer: "all of the above",
+    },
+    {
+    title:    "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+    },
+    {
+    title:   "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log",
+    },
+];
 
 
-//setting the numerical variables for the functions.. scores and timers.. 
+//set counter/scoring variables 
 var score = 0;
 var currentQuestion = -1;
 var timeLeft = 0;
 var timer;
 
 
-//starts the countdown timer once user clicks the 'start' button
+//starts timer
 function start() {
 
 
@@ -44,7 +47,7 @@ document.getElementById("timeLeft").innerHTML = timeLeft;
 timer = setInterval(function() {
     timeLeft--;
     document.getElementById("timeLeft").innerHTML = timeLeft;
-    //proceed to end the game function when timer is below 0 at any time
+    //end game if timer reaches 0
     if (timeLeft <= 0) {
         clearInterval(timer);
         endGame(); 
@@ -56,25 +59,26 @@ next();
 }
 
 
-//stop the timer to end the game 
+//stops timer
 function endGame() {
 clearInterval(timer);
 
 
 var quizContent = `
-<h2>Game over!</h2>
-<h3>You got a ` + score +  ` /100!</h3>
-<h3>That means you got ` + score / 20 +  ` questions correct!</h3>
-<input type="text" id="name" placeholder="First name"> 
-<button onclick="setScore()">Set score!</button>`;
+<h2>Game over</h2>
+
+<input type="text" id="name" placeholder="initials"> 
+<button onclick="setScore()">Save score</button>`;
 
 
 document.getElementById("quizBody").innerHTML = quizContent;
 }
 
 
-//store the scores on local storage
+
+//transfer remaining time into score variable and store the score on local storage
 function setScore() {
+    score = timeLeft;
 localStorage.setItem("highscore", score);
 localStorage.setItem("highscoreName",  document.getElementById('name').value);
 getScore();
@@ -88,7 +92,7 @@ var quizContent = `
 <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
 <h1>` + localStorage.getItem("highscore") + `</h1><br> 
 
-<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+<button onclick="clearScore()">Clear high score</button><button onclick="resetGame()">Play again</button>
 
 `;
 
@@ -97,7 +101,7 @@ document.getElementById("quizBody").innerHTML = quizContent;
 }
 
 
-//clears the score name and value in the local storage if the user selects 'clear score'
+//clears stored high score and initials in the local storage
 function clearScore() {
 localStorage.setItem("highscore", "");
 localStorage.setItem("highscoreName",  "");
@@ -107,7 +111,7 @@ resetGame();
 }
 
 
-//reset the game 
+//reset's the game to beginning 
 function resetGame() {
 clearInterval(timer);
 score = 0;
@@ -120,12 +124,8 @@ document.getElementById("timeLeft").innerHTML = timeLeft;
 
 
 var quizContent = `
-<h1>
-    JavaScript Quiz!
-</h1>
-<h3>
-    Click to play!   
-</h3>
+<h1>JavaScript Quiz!</h1>
+<h3>Click to play!</h3>
 <button onclick="start()">Start!</button>`;
 
 
@@ -133,21 +133,20 @@ document.getElementById("quizBody").innerHTML = quizContent;
 }
 
 
-//deduct 15seconds from the timer if user chooses an incorrect answer
+//removes 15seconds from the timer for incorrect answer
 function incorrect() {
 timeLeft -= 15; 
 next();
 }
 
 
-//increases the score by 20points if the user chooses the correct answer
+//does nothing on correct answer
 function correct() {
-score += 20;
 next();
 }
 
 
-//loops through the questions 
+//goes through each question until the questions end 
 function next() {
 currentQuestion++;
 
@@ -162,17 +161,15 @@ var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
 
 
 for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
-    var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
-    buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+    var buttonCode = "<button onclick=\"[answer]\">[choice]</button>"; 
+    buttonCode = buttonCode.replace("[choice]", questions[currentQuestion].choices[buttonLoop]);
     if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
-        buttonCode = buttonCode.replace("[ANS]", "correct()");
+        buttonCode = buttonCode.replace("[answer]", "correct()");
     } else {
-        buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        buttonCode = buttonCode.replace("[answer]", "incorrect()");
     }
     quizContent += buttonCode
 }
-
-
 
 
 document.getElementById("quizBody").innerHTML = quizContent;
